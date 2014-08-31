94c15ac1d653dd59b5c29c622e5a88deff6e7d5d
christian@americancreativesoftware.com <christian@americancreativesoftware.com>
2014-08-31 11:31:44 -0400

    Initial checkin

---
+++ mvcbase.web.ui/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcbase.web.ui.MvcApplication" Language="C#" %>
---
+++ mvcbase.web.ui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcbase.web.ui.Startup))]
+namespace mvcbase.web.ui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

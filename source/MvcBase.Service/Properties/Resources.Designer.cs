a5f8a41420f35b21d3bbf41c90bcf515d769c648
christian@americancreativesoftware.com <christian@americancreativesoftware.com>
2014-08-31 15:49:40 -0400

    Code cleanup

--- source/MvcBase.Service/Properties/Resources.Designer.cs
+++ source/MvcBase.Service/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace MvcBase.Service.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("mvcbase.service.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MvcBase.Service.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
